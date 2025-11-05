e0dfb5e2ca1bba1112782cd953acefae483adec3
Bhuvan Gowda NC <infigitprojects@gmail.com>
2025-11-05 09:50:59 +0530

    Create user model with Mongoose schema

---
+++ backend/models/user.js
@@ -0,0 +1,10 @@
+// backend/models/User.js
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  password: { type: String, required: true }
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
